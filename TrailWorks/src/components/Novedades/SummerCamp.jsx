98f2dbf2359c9bd8bfc43c4521170a1c4d42c82a
mariodiaz7 <00020322@uca.edu.sv>
2024-08-18 14:40:36 -0600

    Last Update

--- TrailWorks/src/components/Novedades/SummerCamp.jsx
+++ TrailWorks/src/components/Novedades/SummerCamp.jsx
@@ -25,16 +25,16 @@ const SummerCamp = () => {
                     <h1 className="text-4xl md:text-6xl font-bold mb-2 text-center md:text-left text-gray-800">Summer Camp "TrailWorks Academy" 2024</h1>
                     <h2 className='text-3xl md:text-4xl font-semibold mb-2 md:mb-12 text-center md:text-left text-gray-700'>PHOTO RECAP</h2>
                     <p className="text-lg md:text-xl mb-6 text-justify">
-                    El día domingo 4 de agosto tuvimos nuestras primeras clases en apoyo a ECOPROJECT brindadas por uno de los mejores @mauriciorcastaneda
+                    E summer camp 2024 abrio sus puertas a chicos de 4 a 15 años desde finales de mayo a principios de agosto
                     </p>
                     <p className="text-lg md:text-xl mb-6 text-justify">
-                    La pasamos increíble aprendiendo y mejorando desde lo más básico hasta lo avanzado, gracias a ustedes está siendo posible todo esto, les agradecemos riders
+                    La apuesta por la juventud e iniciación temprana del deporte con los niños representa el compromiso de Trailworks por generar cultura y comunidad del ciclismo de montaña. Algunos de estos chicos llevan años  practicando su técnica y an mejorado mucho en poco tiempo
                     </p>
                     <p className="text-lg md:text-xl mb-6 text-justify">
-                    Esperen mucho más y siempre de la mejor calidad
+                    El objetivo del curso de verano es q se diviertan haciendo actividades multidisciplinares con enfoque en ciclismo de montaña.
                     </p>
                     <p className="text-lg md:text-xl mb-6 text-justify">
-                    A darle riders!</p>
+                    Las puertas del curso de verano vuelven abrir en mayo 2025. Mientras tanto clases regulares de lunes a sábado por las tardes de 4:00 pm a 5:30 pm</p>
                 </div>
 
                 {/* Columna del slider de imágenes */}
